18730f3634ee697b72a9c84a12b8c5024aa5745c
Siarhei Blashuk <siarhei.blashuk@outlook.com>
2020-01-20 18:09:58 +0300

    Add candidate component and create basic UI structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {CoreModule} from "./core/core.module";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
+import {CandidatesModule} from "./modules/candidates/candidates.module";
+
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
@@ -10,9 +19,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    CandidatesModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/modules/candidates/candidates.module.ts
+++ src/app/modules/candidates/candidates.module.ts
@@ -1,29 +1,22 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {HeaderComponent} from './header/header.component';
-import {SidebarComponent} from './sidebar/sidebar.component';
+import {CandidateListComponent} from './candidate-list/candidate-list.component';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
-import {CandidatesModule} from "../modules/candidates/candidates.module";
-
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    SidebarComponent
-  ],
+  declarations: [CandidateListComponent],
   imports: [
     CommonModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule,
-    CandidatesModule
+    MatListModule
   ],
   exports: [
-    SidebarComponent,
-    HeaderComponent,
+    CandidateListComponent
   ]
+
 })
-export class CoreModule {
+export class CandidatesModule {
 }
